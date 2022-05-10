56744916ace82edca0450c08155671377add99b8
Aaron Enyetu <aaronenyetu@gmail.com>
2022-05-10 12:24:50 -0700

    adding plant details

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 // import functions and grab DOM elements
 
 import { plants } from './plants.js';
+import { renderPlant } from './utils.js';
 import { renderPlantLI } from './utils.js';
 
 const plantLinks = document.getElementById('plant-links');
+const plantListElem = document.getElementById('plants');
 
 for (let plant of plants) {
     const li = renderPlantLI(plant);
     plantLinks.append(li);
 }
 
+
+for (let plant of plants) {
+  // plantListElem.textContent = '';
+    const plantDiv = renderPlant(plant);
+    plantListElem.append(plantDiv);
+}
 // let state
 
 // set event listeners 
--- utils.js
+++ utils.js
@@ -18,3 +18,32 @@ export function findById(id, arr) {
         }
     }
 }
+
+
+export function renderPlant(plant) {
+    const div = document.createElement('div');
+    div.classList.add('plant');
+
+    const h1 = document.createElement('h1');
+    h1.textContent = plant.name;
+
+    const img = document.createElement('img');
+    img.src = plant.image;
+
+
+    const p = document.createElement('p');
+    p.textContent = `${plant.name} is a cereal ${plant.classification} first domesticated in ${plant.origin} about ${plant.domesticated} years ago  `;
+
+    const h2 = document.createElement('h2');
+    h2.textContent = 'Types';
+    console.log(plant.subspecies);
+    const ul = document.createElement('ul');
+    for (let species of plant.subspecies) {
+        const li = document.createElement('li');
+        li.textContent = species;
+        ul.append(li);
+       
+    }
+    div.append(h1, img, p, h2, ul);
+    return div;
+}
